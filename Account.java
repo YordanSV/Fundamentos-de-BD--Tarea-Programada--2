be1cdb2666acfe8d7bf0c0adcf9d03c1efb790d3
Yordan Salazar <yordan10_pv@hotmail.com>
2023-05-31 18:17:36 -0600

    Se han creado funciones get y set

--- Account.java
+++ Account.java
@@ -5,14 +5,26 @@ public class Account {
     String accountNumber;
     String identity;
     String amount;
-    
-    public Account(String accountNumber, String identity, String amount){
+
+    public Account(String accountNumber, String identity, String amount) {
         this.accountNumber = accountNumber;
         this.identity = identity;
         this.amount = amount;
     }
 
-    public String getAccountNumber(){
+    public String getIdentity() {
+        return identity;
+    }
+
+    public String getAccountNumber() {
         return accountNumber;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String modify) {
+        amount = Integer.toString(Integer.parseInt(amount) + Integer.parseInt(modify));
     }
 }
